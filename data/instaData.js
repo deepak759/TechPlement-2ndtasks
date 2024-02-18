cf5ee476f6591e50701564bbff82ab371e303a46
deepak759 <83724070+deepak759@users.noreply.github.com>
2024-02-18 17:14:57 +0530

    data updated

--- data/instaData.js
+++ data/instaData.js
@@ -9,7 +9,7 @@ const instaData = {
   posts: [
     {
       imageUrl:
-        "https://scontent.fdel2-3.fna.fbcdn.net/v/t39.30808-6/375578569_866004608225301_6335499369201609730_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=3635dc&_nc_ohc=5wyc_gaflTMAX-Xf6Yp&_nc_ht=scontent.fdel2-3.fna&oh=00_AfC37AUXLa1SnkxuXyF_AFo9HILcYb3gXf2BJZpuTtc11w&oe=65D27D10",
+        "https://pbs.twimg.com/media/Fv507w8aYAY7QQL?format=jpg&name=medium",
       likes: "549k",
       share: "456k",
       commentsCount: "325k",
@@ -19,7 +19,7 @@ const instaData = {
     },
     {
       imageUrl:
-        "https://scontent.fdel2-2.fna.fbcdn.net/v/t39.30808-6/375574746_866004594891969_5034632541330816182_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=3635dc&_nc_ohc=juM2kXPo4l4AX8dRNhi&_nc_ht=scontent.fdel2-2.fna&oh=00_AfC6gT9LRbyYewS6rup0M8RY3M47AG4jy6soi19LMjTYDA&oe=65D24B49",
+      "https://pbs.twimg.com/media/Fscii0gaUAEdO-T?format=jpg&name=large",
       likes: "695k",
       share: "456k",
       commentsCount: '35k',
@@ -29,7 +29,7 @@ const instaData = {
     },
     {
       imageUrl:
-        "https://scontent.fdel2-3.fna.fbcdn.net/v/t39.30808-6/375567516_866004574891971_2109821075223960810_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=3635dc&_nc_ohc=W-TylPeG3tcAX_lOoph&_nc_ht=scontent.fdel2-3.fna&oh=00_AfDuE7SpnCEXri2LP18VCDPYCOmqofZPi26f0hSytRTfKw&oe=65D307D3",
+        "https://pbs.twimg.com/media/Fr_dly2XoAIzIXu?format=jpg&name=medium",
         likes: "65k",
         share: "496k",
         commentsCount: '35k',
@@ -39,7 +39,7 @@ const instaData = {
     },
     {
       imageUrl:
-        "https://scontent.fdel2-3.fna.fbcdn.net/v/t39.30808-6/358127303_836711774487918_2786667831458035462_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=ZvV-aIWaU1EAX-1OGFR&_nc_ht=scontent.fdel2-3.fna&oh=00_AfAI4HLvkbnmdgVPZpIEjtOP0TfoFafZ_uNI7xMvzISKYg&oe=65D1B501",
+        "https://pbs.twimg.com/media/FmBTSVeaEAI0fRX?format=jpg&name=large",
         likes: "495k",
         share: "956k",
         commentsCount: '355k',
@@ -49,7 +49,7 @@ const instaData = {
     },
     {
       imageUrl:
-        "https://scontent.fdel2-3.fna.fbcdn.net/v/t39.30808-6/347404681_785569573155717_847268627516547115_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=3635dc&_nc_ohc=NCitRBfs2JgAX9p3lae&_nc_ht=scontent.fdel2-3.fna&oh=00_AfC7_adDw40Sgo6BX-hZJKrNDeTjqLlBpUF9wkz86C1Zow&oe=65D1B0A9",
+        "https://pbs.twimg.com/media/FlX3N2qacAAsM9g?format=jpg&name=large",
         likes: "195k",
         share: "456k",
         commentsCount: '305k',
@@ -59,7 +59,7 @@ const instaData = {
     },
     {
       imageUrl:
-        "https://scontent.fdel2-3.fna.fbcdn.net/v/t39.30808-6/347593015_765060948529248_56537122113261583_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=J0KxKCHvYtwAX-K3vkQ&_nc_ht=scontent.fdel2-3.fna&oh=00_AfCqkB3k23pd_AGz0fZcNUvfwmAfjv2KYB-U275BvZQv1w&oe=65D1B8ED",
+        "https://pbs.twimg.com/media/FiJpCJHUoAA9h4T?format=jpg&name=medium",
         likes: "69k",
         share: "486k",
         commentsCount: '351k',
@@ -69,7 +69,7 @@ const instaData = {
     },
     {
       imageUrl:
-        "https://scontent.fdel2-3.fna.fbcdn.net/v/t39.30808-6/349123350_1868532870185228_2903118077257261780_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=Mn6_8JL8604AX_xbroP&_nc_ht=scontent.fdel2-3.fna&oh=00_AfBjXUzzPY9lhrROC0C7DdbV9UevXxkx7gt1SKUYz9WA_g&oe=65D3687A",
+        "https://pbs.twimg.com/media/FVRUhKCaQAENmj_?format=jpg&name=large",
         likes: "695k",
         share: "456k",
         commentsCount: '35k',
@@ -79,7 +79,7 @@ const instaData = {
     },
     {
       imageUrl:
-        "https://scontent.fdel2-3.fna.fbcdn.net/v/t39.30808-6/346090794_195882673389755_7765453038202639416_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=A2z6441PxwIAX8AEuMK&_nc_ht=scontent.fdel2-3.fna&oh=00_AfCWJ3-RAJL6OG9-BlQWNGhq2ukJHQ_oWqI709jEAY83YQ&oe=65D1B443",
+        "https://pbs.twimg.com/media/FRqxRwZVIAAaggk?format=jpg&name=large",
         likes: "69k",
         share: "456k",
         commentsCount: '35k',
@@ -89,7 +89,7 @@ const instaData = {
     },
     {
       imageUrl:
-        "https://scontent.fdel2-2.fna.fbcdn.net/v/t39.30808-6/342624990_558891202995586_6491384851168697307_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=dt2-yu_JsvMAX-Ujj6Q&_nc_ht=scontent.fdel2-2.fna&oh=00_AfBWft9r3TIQJXGz8fzqDaDVcFNILrwKET7_DM8R0cM6Lw&oe=65D29173",
+        "https://pbs.twimg.com/media/FO_ogf8VsAERXfV?format=jpg&name=medium",
         likes: "65k",
         share: "56k",
         commentsCount: '35k',
@@ -99,7 +99,7 @@ const instaData = {
     },
     {
       imageUrl:
-        "https://scontent.fdel2-3.fna.fbcdn.net/v/t39.30808-6/342049534_235044039072591_2694082534622318043_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=dd5e9f&_nc_ohc=FrTRHIbHr4MAX8rWFpw&_nc_ht=scontent.fdel2-3.fna&oh=00_AfCKnI7zyjcc-rr1lhN92sECQoeTopEJQKufqZpxqNeSbA&oe=65D2B863",
+        "https://pbs.twimg.com/media/FOMqmjIVQAISqpN?format=jpg&name=medium",
         likes: "695k",
         share: "426k",
         commentsCount: '35k',
